77794159005461b87517ca20a5c0ff8e557abb87
servostar <sven.vogel123@web.de>
2025-01-03 23:19:11 +0100

    feat: add configurable styling

--- shuttle.sh
+++ shuttle.sh
@@ -3,6 +3,9 @@
 # Escape sequences
 # shellcheck source-path=src
 . "./lib/ecma48.sh"
+# Styles
+# shellcheck source-path=lib
+. "./lib/styles.sh"
 # Logging functions.
 # shellcheck source-path=src
 . "./lib/log.sh"
